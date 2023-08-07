f6a61e0772ef42a96ddf9a8b2b3ed0e4fc03c8b4
avach <avachaspati97@gmail.com>
2023-08-08 01:20:01 +0530

    added streaming model in triton (Temp)

--- model_repository/santacoder_huggingface_stream/1/model.py
+++ model_repository/santacoder_huggingface_stream/1/model.py
@@ -1,5 +1,5 @@
 import triton_python_backend_utils as pb_utils
-from transformers import AutoModelForCausalLM, AutoTokenizer
+from transformers import AutoModelForCausalLM, AutoTokenizer, TextStreamer
 import torch
 import os
 import traceback
@@ -19,17 +19,21 @@ class TritonPythonModel:
 
     def execute(self, requests):
         responses = []
+        response_senders = []
         inputs = []
         eos_token = self.tokenizer.convert_ids_to_tokens(self.tokenizer.eos_token_id)
         try:
             for request in requests:
                 in_text = pb_utils.get_input_tensor_by_name(request, "input")
                 in_text = in_text.as_numpy()[0][0].decode('utf-8')
+                response_senders.apppend(request.get_response_sender())
             
             tokens = self.tokenizer(inputs, padding=True, return_tensors="pt").to(self.device)
-            outputs = self.model.generate(**tokens,pad_token_id=self.tokenizer.eos_token_id,min_new_tokens=0,max_new_tokens=25)
+            streamer = TextStreamer(self.tokenizer)
+            outputs = self.model.generate(**tokens,streamer=streamer, pad_token_id=self.tokenizer.eos_token_id,min_new_tokens=0,max_new_tokens=25)
+            print(outputs)
             results = self.tokenizer.batch_decode(outputs)
-            
+
             ## Removing Response for Empty Input Strings
             for i in range(len(results)):
                 inference_response = pb_utils.InferenceResponse(output_tensors=[
