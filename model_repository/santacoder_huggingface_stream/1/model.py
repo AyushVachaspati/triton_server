c51f481dbbf5c982aae25127d1d367f672bb9e67
avach <avachaspati97@gmail.com>
2023-08-13 00:27:59 +0530

    Added Batched Streaming Model using GRPC

--- model_repository/santacoder_huggingface_stream/1/model.py
+++ model_repository/santacoder_huggingface_stream/1/model.py
@@ -1,11 +1,39 @@
 import triton_python_backend_utils as pb_utils
-from transformers import AutoModelForCausalLM, AutoTokenizer,  TextIteratorStreamer
+from transformers import AutoModelForCausalLM, AutoTokenizer
 import torch
 import os
 import traceback
 import numpy as np
 import time
 from threading import Thread
+
+
+# Output Streamer Implements the put() and end() methods from the BaseStreamer interface in HuggingFace Transformers libaray.
+class OutputStreamer():
+    def __init__(self, tokenizer: "AutoTokenizer", skip_prompt: bool = False, response_senders = []):
+        self.tokenizer = tokenizer
+        self.skip_prompt = skip_prompt
+        self.response_senders = response_senders
+        self.next_tokens_are_prompt = True
+
+    def put(self, value):
+        if self.skip_prompt and self.next_tokens_are_prompt:
+            self.next_tokens_are_prompt = False
+            return
+        outputs = self.tokenizer.batch_decode(value)
+        for i in range(len(outputs)):
+            inference_response = pb_utils.InferenceResponse(output_tensors=[
+                    pb_utils.Tensor("output",np.array([outputs[i]],dtype=object))
+                ])
+            self.response_senders[i].send(inference_response)
+        
+    def end(self):
+        for response_sender in self.response_senders:
+             response_sender.send(pb_utils.InferenceResponse(output_tensors=[
+                    pb_utils.Tensor("output",np.array(["<|Endoftext|>"],dtype=object))
+                ]),flags=pb_utils.TRITONSERVER_RESPONSE_COMPLETE_FINAL)
+
+
 
 class TritonPythonModel:
     def initialize(self, args):
@@ -20,27 +48,20 @@ class TritonPythonModel:
         print("Model Loaded")
 
     def execute(self, requests):
-        responses = []
-        response_sender = []
+        response_senders = []
         inputs = []
+
         for request in requests:
             in_text = pb_utils.get_input_tensor_by_name(request, "input")
-            in_text = in_text.as_numpy()[0].decode('utf-8')
-            streamer = TextIteratorStreamer(self.tokenizer,skip_prompt=True)
-            inputs = self.tokenizer([in_text],return_tensors="pt").to(self.device)
-            # Run the generation in a separate thread, so that we can fetch the generated text in a non-blocking way.
-            generation_kwargs = dict(inputs, streamer=streamer, max_new_tokens=256)
-            thread = Thread(target=self.model.generate, kwargs=generation_kwargs)
-            thread.start()
-            response_sender = request.get_response_sender()
-            for new_text in streamer:
-                inference_response = pb_utils.InferenceResponse(output_tensors=[
-                    pb_utils.Tensor("output",np.array([new_text],dtype=object))
-                ])
-                response_sender.send(inference_response)
-            response_sender.send(pb_utils.InferenceResponse(output_tensors=[
-                    pb_utils.Tensor("output",np.array(["<|Endoftext|>"],dtype=object))
-                ]),flags=pb_utils.TRITONSERVER_RESPONSE_COMPLETE_FINAL)
+            in_text = in_text.as_numpy()[0][0].decode('utf-8')
+            inputs.append(in_text)
+            response_senders.append(request.get_response_sender())
+
+        streamer = OutputStreamer(self.tokenizer,skip_prompt=False,response_senders=response_senders)
+        inputs = self.tokenizer(inputs,return_tensors="pt").to(self.device)
+        self.model.generate(**inputs,streamer=streamer,
+                            min_new_tokens=0,max_new_tokens=50,pad_token_id=self.tokenizer.eos_token_id)
+        
         return None
     
         # return responses
