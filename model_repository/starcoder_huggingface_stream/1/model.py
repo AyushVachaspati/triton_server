87e17c63c90db8077ad1be16aba11f7835915752
avach <avachaspati97@gmail.com>
2023-08-15 17:34:44 +0530

    Fixed Stream Model

--- model_repository/starcoder_huggingface_stream/1/model.py
+++ model_repository/starcoder_huggingface_stream/1/model.py
@@ -6,6 +6,33 @@ import os
 import traceback
 import numpy as np
 
+
+# Output Streamer Implements the put() and end() methods from the BaseStreamer interface in HuggingFace Transformers libaray.
+class OutputStreamer():
+    def __init__(self, tokenizer: "AutoTokenizer", skip_prompt: bool = False, response_senders = []):
+        self.tokenizer = tokenizer
+        self.skip_prompt = skip_prompt
+        self.response_senders = response_senders
+        self.next_tokens_are_prompt = True
+
+    def put(self, value):
+        if self.skip_prompt and self.next_tokens_are_prompt:
+            self.next_tokens_are_prompt = False
+            return
+        outputs = self.tokenizer.batch_decode(value)
+        for i in range(len(outputs)):
+            inference_response = pb_utils.InferenceResponse(output_tensors=[
+                    pb_utils.Tensor("output",np.array([outputs[i]],dtype=object))
+                ])
+            self.response_senders[i].send(inference_response)
+        
+    def end(self):
+        for response_sender in self.response_senders:
+             response_sender.send(pb_utils.InferenceResponse(output_tensors=[
+                    pb_utils.Tensor("output",np.array(["<|Endoftext|>"],dtype=object))
+                ]),flags=pb_utils.TRITONSERVER_RESPONSE_COMPLETE_FINAL)
+
+
 class TritonPythonModel:
     def initialize(self, args):
         print("Loading Model")
@@ -20,29 +47,21 @@ class TritonPythonModel:
         print("Model Loaded")
 
     def execute(self, requests):
-        responses = []
+        response_senders = []
         inputs = []
-        eos_token = self.tokenizer.convert_ids_to_tokens(self.tokenizer.eos_token_id)
-        try:
-            for request in requests:
-                in_text = pb_utils.get_input_tensor_by_name(request, "input")
-                in_text = in_text.as_numpy()[0][0].decode('utf-8')
-                inputs.append(in_text)
-            
-            tokens = self.tokenizer(inputs, padding=True, return_tensors="pt").to(self.device)
-            outputs = self.model.generate(**tokens,pad_token_id=self.tokenizer.eos_token_id,
-                                          min_new_tokens=0,max_new_tokens=25)
-            results = self.tokenizer.batch_decode(outputs)
-            
-            ## Removing Response for Empty Input Strings
-            for i in range(len(results)):
-                final_result = results[i].rstrip(eos_token)
-                inference_response = pb_utils.InferenceResponse(output_tensors=[
-                    pb_utils.Tensor("output",np.array([final_result],dtype=object))
-                ])
-                responses.append(inference_response)
-        except:
-            traceback.print_exc()
+
+        for request in requests:
+            in_text = pb_utils.get_input_tensor_by_name(request, "input")
+            in_text = in_text.as_numpy()[0][0].decode('utf-8')
+            inputs.append(in_text)
+            response_senders.append(request.get_response_sender())
+
+        streamer = OutputStreamer(self.tokenizer,skip_prompt=False,response_senders=response_senders)
+        inputs = self.tokenizer(inputs,return_tensors="pt").to(self.device)
+        self.model.generate(**inputs,streamer=streamer,
+                            min_new_tokens=0,max_new_tokens=50,pad_token_id=self.tokenizer.eos_token_id)
+        
+        return None
         
         return responses
 
