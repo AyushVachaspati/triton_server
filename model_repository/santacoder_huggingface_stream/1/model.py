245189fbc216b99d4a53f24f637272741e96812d
avach <avachaspati97@gmail.com>
2023-08-17 15:40:06 +0530

    Updated Models to Support Streaming for DIfferent Lengths input and output

--- model_repository/santacoder_huggingface_stream/1/model.py
+++ model_repository/santacoder_huggingface_stream/1/model.py
@@ -11,28 +11,41 @@ from threading import Thread
 
 # Output Streamer Implements the put() and end() methods from the BaseStreamer interface in HuggingFace Transformers libaray.
 class OutputStreamer():
-    def __init__(self, tokenizer: "AutoTokenizer", skip_prompt: bool = False, response_senders = []):
+    def __init__(self, tokenizer: "AutoTokenizer", skip_prompt: bool = False, response_senders = [], eos_token = "<|endoftext|>"):
         self.tokenizer = tokenizer
         self.skip_prompt = skip_prompt
         self.response_senders = response_senders
         self.next_tokens_are_prompt = True
-
+        self.eos_token = eos_token
+    
     def put(self, value):
         if self.skip_prompt and self.next_tokens_are_prompt:
             self.next_tokens_are_prompt = False
             return
+        
         outputs = self.tokenizer.batch_decode(value)
         for i in range(len(outputs)):
-            inference_response = pb_utils.InferenceResponse(output_tensors=[
+            try:
+                inference_response = pb_utils.InferenceResponse(output_tensors=[
                     pb_utils.Tensor("output",np.array([outputs[i]],dtype=object))
                 ])
-            self.response_senders[i].send(inference_response)
+                if(outputs[i]==self.eos_token):
+                    self.response_senders[i].send(inference_response, flags=pb_utils.TRITONSERVER_RESPONSE_COMPLETE_FINAL)
+                else:
+                    self.response_senders[i].send(inference_response)
+            except:
+                # Trying to send response on closed connection
+                pass
         
     def end(self):
         for response_sender in self.response_senders:
-             response_sender.send(pb_utils.InferenceResponse(output_tensors=[
-                    pb_utils.Tensor("output",np.array(["<|endoftext|>"],dtype=object))
-                ]),flags=pb_utils.TRITONSERVER_RESPONSE_COMPLETE_FINAL)
+            try:
+                response_sender.send(pb_utils.InferenceResponse(output_tensors=[
+                        pb_utils.Tensor("output",np.array([self.eos_token],dtype=object))
+                    ]),flags=pb_utils.TRITONSERVER_RESPONSE_COMPLETE_FINAL)
+            except:
+                # Trying to send response on closed connection
+                pass
 
 
 
@@ -59,10 +72,12 @@ class TritonPythonModel:
             inputs.append(in_text)
             response_senders.append(request.get_response_sender())
 
-        streamer = OutputStreamer(self.tokenizer,skip_prompt=False,response_senders=response_senders)
-        inputs = self.tokenizer(inputs,return_tensors="pt").to(self.device)
+        streamer = OutputStreamer(self.tokenizer,skip_prompt=True,response_senders=response_senders)
+        inputs = self.tokenizer(inputs,padding=True,return_tensors="pt").to(self.device)
         self.model.generate(**inputs,streamer=streamer,
-                            min_new_tokens=0,max_new_tokens=50,pad_token_id=self.tokenizer.eos_token_id)
+                            min_new_tokens=0,max_new_tokens=500,
+                            pad_token_id=self.tokenizer.eos_token_id,
+                            eos_token_id=self.tokenizer.eos_token_id)
         
         return None
     
